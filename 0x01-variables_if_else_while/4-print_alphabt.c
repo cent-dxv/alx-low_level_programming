f1c66e34847e86f35c7294a222f0b1e363097887
cent-dxv <centdxv@gmail.com>
2022-03-11 11:48:43 -0800

    issue fixed 🪲

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
-
+/* description */
 int main(void)
 {
 	char alp = 'a';
+
 	for ( ; alp <= 'z' ; alp++)
-		if(alp != 'q' && alp != 'e' )
+		if (alp != 'q' && alp != 'e')
 			putchar(alp);
 
 	putchar('\n');
 
 
-	return(0);
+	return (0);
 
 }
 
