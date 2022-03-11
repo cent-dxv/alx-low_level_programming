f75cab7e92b1ebe5a746b972df3941b8722bb487
cent-dxv <centdxv@gmail.com>
2022-03-11 11:40:38 -0800

    issue fixed 🪲

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
-int main (void)
+int main(void)
 {
 
 
 	int i;
-	for (i = 0 ; i <= 9 ;  i ++ )
+
+	for (i = 0 ; i <= 9 ;  i++)
 	{
-	putchar(i + '0' );
-	if (i == 9 )
-	continue; 
+	putchar(i + '0');
+	if (i == 9)
+	continue;
 	putchar(',');
 	putchar(' ');
 	}
