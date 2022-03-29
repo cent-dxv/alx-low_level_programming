3d8bd16b67391f9bed5c34cd229cbdda965df86d
cent-dxv <80540398+cent-dxv@users.noreply.github.com>
2022-03-29 09:24:07 +0300

    Update 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,25 +1,32 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * _strspn - gets lengthof a prefix substring
- * @s: string to check
- * @accept: string to check against
- *
- * Return: number of bytes of s in accept
- */
+  * _strspn - search a string for a set of bytes
+  * @s: source string
+  * @accept: accepted string
+  *
+  * Return: number of bytes in the init segment
+  */
 unsigned int _strspn(char *s, char *accept)
 {
-unsigned int i, j; 
+	unsigned int a = 0, b, t = 0;
 
-	for (i = 0; s[i]; i++)
+	while (accept[a])
 	{
-		for (j = 0; accept[j]; j++)
+		b = 0;
+
+		while (s[b] != 32)
 		{
-			if (s[i] == accept[j])
-				break;
+			if (accept[a] == s[b])
+			{
+				t++;
+			}
+
+			b++;
 		}
-		if (!accept[j])
-			break;
+
+		a++;
 	}
-	return (i);
+
+	return (t);
 }
